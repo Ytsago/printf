6933a1d13a28ac4d29e83a3bfae228e5bcde7458
Sebastien Cros <secros@z2r9p3.42lyon.fr>
2024-11-20 15:59:21 +0100

    norm OK

--- libft/ft_strjoin.c
+++ libft/ft_strjoin.c
@@ -6,7 +6,7 @@
 /*   By: secros <secros@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/09 17:36:35 by secros            #+#    #+#             */
-/*   Updated: 2024/11/20 10:37:48 by secros           ###   ########.fr       */
+/*   Updated: 2024/11/20 15:59:08 by secros           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,9 +20,9 @@ char	*ft_strjoin(char const *s1, char const *s2)
 
 	len1 = 0;
 	len2 = 0;
-	while(s1[len1])
+	while (s1[len1])
 		len1++;
-	while(s2[len2])
+	while (s2[len2])
 		len2++;
 	fs = (char *) malloc(sizeof(char) * (len1 + len2 + 1));
 	if (!fs)
@@ -30,7 +30,7 @@ char	*ft_strjoin(char const *s1, char const *s2)
 	fs[len1 + len2] = '\0';
 	while ((len2--))
 		fs[len1 + len2] = s2[len2];
-	while(len1--)
+	while (len1--)
 		fs[len1] = s1[len1];
 	return (fs);
 }
